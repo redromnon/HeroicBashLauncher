af615db8719f5976acdef8199afe9282049da3e6
Rishabh Moharir <74495920+redromnon@users.noreply.github.com>
2022-02-03 11:11:20 +0530

    Updated version no.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 ###################
 
-#VERSION 1.8.0
+#VERSION 1.8.1
 
 #Thanks for using Heroic Bash Launcher - A simple program that allows you to launch any of your Epic Store games from literally anywhere on Linux!
 

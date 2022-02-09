148e3a3f7d17764972adfc698a45a5484f7f3b3e
Redromnon <rishimoharir@gmail.com>
2022-02-09 16:02:38 +0530

    Program execution depends on no. of arguments
    Earlier arguments had to be included with the bash command, now only depends on no. of args

--- func/HeroicBashLauncher.py
+++ func/HeroicBashLauncher.py
@@ -4,21 +4,18 @@ import os,sys
 from frombash import frombash
 from listinstalled import listinstalled
 
-#Take arguments
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
-arg3 = sys.argv[3]
 
 if os.path.exists(os.path.expanduser("~") + "/.config/legendary/installed.json") == True:
 
-    #If arg1 is "", then proceed to create launch files for all games
-    #   else, update paramters of a game
-    if arg1 == "":
+    #If len of arguments is 1 (no extra arguements), then proceed to create launch files for all games
+    #   else, update parameters of a game through launch file
+
+    if len(sys.argv) == 1: #Only name of file as default argument
     
         listinstalled()
         os.system('zenity --info --title="Process Finished" --text="Launch files stored in GameFiles folder.\n\nHave fun gaming!" --width=200')
     else:
-        frombash(arg1, arg2, arg3)
+        frombash(sys.argv[1], sys.argv[2], sys.argv[3])
 else:
 
     os.system('zenity --error --title="Process Stopped" --text="Looks like you have not installed Heroic Games Launcher or installed any game\n\nPlease consider doing so and try again." --width=200')

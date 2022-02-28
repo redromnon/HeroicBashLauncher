5c2dc8ebd63d9274d54ca1374c4b73a92ed5da6e
Ross Carran <carran.ross@gmail.com>
2022-03-01 01:53:58 +1300

    Update appimage_binary.py
    Set a static path for legendary.

--- func/appimage_binary.py
+++ func/appimage_binary.py
@@ -1,34 +1,10 @@
 #For AppImage, check if alternavtive binary (Legendary) is added.
 #If not, check folder under /tmp/ that includes path to the binaries.
 
-import os, json, re 
+import os, json
 
 def getlegendaryappimage():
     
-    legendary_path = ""
-
-    #Path to heroic's  where each installed game's json is stored
-    heroicconfigpath = os.path.expanduser("~") + "/.config/heroic/config.json"
-
-    #Path to tmp dir
-    list = os.listdir('/tmp/')
-
-    #Convert config json to dict
-    with open(heroicconfigpath) as p:
-      heroicconfig = json.load(p)
-
-    #If legendary binary doesn't exist, check for temp folder
-    if heroicconfig["defaultSettings"]["altLegendaryBin"] == "":
-        
-        for i in list:
-            if re.search("Heroic", i) != None:
-                #print(i)
-                legendary_path = '/tmp/' + i + '/resources/app.asar.unpacked/build/bin/linux/legendary '
-                break
-    else:
-
-        legendary_path = heroicconfig["defaultSettings"]["altLegendaryBin"] + " "
+    legendary_path = os.getcwd() + "/legendary "
 
     return legendary_path
-
-

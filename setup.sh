2cc5f2abdaa28994076367aeb3cc60fad359f041
Redromnon <rishimoharir@gmail.com>
2022-04-02 06:12:00 +0530

    Update to v2.3.0
    Run executable and create log

--- setup.sh
+++ setup.sh
@@ -1,23 +1,29 @@
 #!/usr/bin/bash
 
-###################
+#####
 
-#VERSION 1.9.1
+#VERSION 2.3.0
 
-#Thanks for using Heroic Bash Launcher - A simple program that allows you to launch any of your Epic Store games from literally anywhere on Linux!
+#Thanks for using Heroic Bash Launcher - A simple program that allows you to launch any of your Heroic games (Epic and GOG) from literally 
+#anywhere on Linux!
 
 #Do note that this project is open-source and is under the GPL-3.0 License. For more information, you can check out the LICENSE.md file.
 #Moreover, this software does not come without any warranty.
 
 #Hope you liked my little project! Have fun gaming!
 
-#-- By Redromnon
+#-- Redromnon
 
-###################
+#####
 
-#Check if Zenity is installed
-#(echo "Checking if Zenity is installed..." ; zenity --version) || (echo "Zenity not installed, please consider installing it.")
+exec > HeroicBashLauncher.log 2>&1
 
-#Run the Heroic Bash Launcher program
-(python3 func/HeroicBashLauncher.py)\
- || zenity --error --title="Process Failed" --text="HeroicBashLauncher failed to create scripts.\n\nPlease check your console for the error and consider reporting it as an issue on Github." --width=400
+EXE=HeroicBashLauncher
+if [ -f "$EXE" ]; then
+    echo "Using $EXE Base/AppImage version..." ; ./HeroicBashLauncher
+else
+    echo "Using $EXE Flatpak version..." ; (cd GameFiles && ./HeroicBashLauncher)
+fi
+
+
+

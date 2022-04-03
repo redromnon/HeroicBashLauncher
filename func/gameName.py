d1605510bfce3b20e90a7dad757c33f746d5d0c1
Redromnon <rishimoharir@gmail.com>
2022-04-03 10:22:39 +0530

    Include certain characters in game names

--- func/gameName.py
+++ func/gameName.py
@@ -1,11 +1,14 @@
 #Remove special characters from game names (caused errors when using encode()/decode())
 def rspchar(realgamename):
-    
+
     name = ""
+
+    #Include the following characters
+    includechar = [" ", "'", ":", "(", ")"]
 
     for char in realgamename:
 
-        if char.isalnum() == True or char == " ":
+        if char.isalnum() == True or char in includechar:
 
             name = name + char
 

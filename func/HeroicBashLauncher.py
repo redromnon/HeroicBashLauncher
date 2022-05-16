029e47c9bae37c4888bb69f66bb086888cb85f5a
Redromnon <rishimoharir@gmail.com>
2022-05-16 20:07:39 +0530

    Update zenity dialogs

--- func/HeroicBashLauncher.py
+++ func/HeroicBashLauncher.py
@@ -20,11 +20,14 @@ if (os.path.exists(configpath.legendaryinstalledpath) == True or os.path.exists(
 
     if len(sys.argv) == 1: #Only name of file as default argument
     
+        if "deck" in os.path.expanduser("~"):
+            os.system('zenity --info --title="Process Starting" --text="This may take a while depending on your internet connection and number of games." --width=300 --timeout=3')
+        
         listinstalled()
 
         #Don't create AddToSteam script if Steam Deck 
         if "deck" in os.path.expanduser("~"):
-            os.system('zenity --info --title="Process Finished" --text="Launch scripts stored in GameFiles folder\n\nYour games have been synced to Steam\n\nHave fun gaming!" --width=300')
+            os.system('zenity --info --title="Process Finished" --text="Launch scripts stored in GameFiles folder\n\nYour games have been synced to Steam\n\nHave fun gaming!" --width=300 --timeout=3')
         else:
             os.system('zenity --info --title="Process Finished" --text="Launch scripts stored in GameFiles folder\n\nYou can sync games to Steam via AddToSteam\n\nHave fun gaming!" --width=300')
             print("\nCreating AddToSteam script...")
@@ -40,7 +43,7 @@ if (os.path.exists(configpath.legendaryinstalledpath) == True or os.path.exists(
                 addtosteam(i)
 
             os.system('zenity --info --title="Process Finished" --text="Check AddToSteam.log for details." --width=300') 
-    else: #Launch the game
+    else: #Update launch script
         createlaunchfile(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
 elif checkzenity != 0:
     

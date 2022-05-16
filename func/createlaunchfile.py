6098db8e7ae8b2a760ad76127011b6b12844e342
Redromnon <rishimoharir@gmail.com>
2022-05-16 20:02:41 +0530

    Wait 10 sec for game to launch

--- func/createlaunchfile.py
+++ func/createlaunchfile.py
@@ -79,7 +79,7 @@ def createlaunchfile(gamename, appname, gamejson, gametype):
     ({launchcommand} || (echo "---CANNOT CONNECT TO NETWORK. RUNNING IN OFFLINE MODE---" ; {offline_launchcommand})) || (zenity --error --title="Error" --text="Failed to launch ' + {game_name} + '\n\nPlease check the game log under /logs/ for the error and consider reporting it as an issue on GitHub." --width=200; exit)
 
     #Wait for game to launch
-    sleep 6
+    sleep 10
     
     while [ 1 ]
     do

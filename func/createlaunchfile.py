922de6bf2907c1019b635c30723584f83a4971dd
Redromnon <rishimoharir@gmail.com>
2022-05-17 13:44:05 +0530

    [Flatpak] Create separate launch script for running games
    The idea here is avoid any glibc compatibility issues in the future and update the launch commands if changed in Heroic.
    There's a separate launch script - launchflatpakgame - that handles the launching of games.
    This applies only to Flatpak users.

--- func/createlaunchfile.py
+++ func/createlaunchfile.py
@@ -16,23 +16,21 @@ def createlaunchfile(gamename, appname, gamejson, gametype):
     simplified_gamename = filegamename(gamename)
 
     #Creating the game file name
+    if "GameFiles" in os.getcwd():#select parent dir
+        executablepath = os.path.dirname(os.getcwd()) + '/HeroicBashLauncher' + ' "' + gamename + '" "' + appname + '" "' + gamejson + '" "' + gametype + '"' 
+        gameFilepath = os.getcwd() + "/" + simplified_gamename + ".sh"
+        flatpakgamescriptpath = os.getcwd() + "/launchflatpakgame.sh"
+    else:#launching from setup.sh
+        executablepath = os.getcwd() + '/HeroicBashLauncher' + ' "' + gamename + '" "' + appname + '" "' + gamejson + '" "' + gametype + '"'
+        gameFilepath = os.getcwd() + "/GameFiles/" + simplified_gamename + ".sh"
+        flatpakgamescriptpath = os.getcwd() + "/GameFiles/launchflatpakgame.sh"
+    
     if configpath.is_flatpak == False:
-
-        if "GameFiles" in os.getcwd():#select parent dir
-            executablepath = os.path.dirname(os.getcwd()) + '/HeroicBashLauncher' + ' "' + gamename + '" "' + appname + '" "' + gamejson + '" "' + gametype + '"' 
-            gameFilepath = os.getcwd() + "/" + simplified_gamename + ".sh"
-        else:#launching from setup.sh
-            executablepath = os.getcwd() + '/HeroicBashLauncher' + ' "' + gamename + '" "' + appname + '" "' + gamejson + '" "' + gametype + '"'
-            gameFilepath = os.getcwd() + "/GameFiles/" + simplified_gamename + ".sh"
+        launchflatpakgame = ''
     else:
-        
-        if "GameFiles" in os.getcwd():#select parent dir
-            executablepath = 'flatpak run --command=' + os.path.dirname(os.getcwd()) + '/HeroicBashLauncher' + ' com.heroicgameslauncher.hgl' + ' "' + gamename + '" "' + appname + '" "' + gamejson + '" "' + gametype + '"' 
-            gameFilepath = os.getcwd() + "/" + simplified_gamename + ".sh"
-        else:#launching from setup.sh
-            executablepath = 'flatpak run --command=' + os.getcwd() + '/HeroicBashLauncher' ' com.heroicgameslauncher.hgl' + ' "' + gamename + '" "' + appname + '" "' + gamejson + '" "' + gametype + '"' 
-            gameFilepath = os.getcwd() + "/GameFiles/" + simplified_gamename + ".sh"
+        launchflatpakgame = 'flatpak run --command=./launchflatpakgame.sh com.heroicgameslauncher.hgl' 
 
+    ####################################################################################################################
     #Launch Script Format
     launch_script = ("""
     #!/bin/bash 
@@ -50,8 +48,22 @@ def createlaunchfile(gamename, appname, gamejson, gametype):
     #Override launch parameters
     {executable_path}
 
-    """).format(logname = simplified_gamename,game_name = gamename, game_type = gametype, app_name = appname, executable_path = executablepath)
+    {launch_game_in_flatpak}
 
+    """).format(logname = simplified_gamename,game_name = gamename, game_type = gametype, app_name = appname, 
+                executable_path = executablepath, launch_game_in_flatpak = launchflatpakgame)
+
+    
+    #Flatpak Game Script Format
+    launch_flatpak_script = ("""
+    #!/bin/bash
+
+    #Currently created launch script for {game_name} ({app_name}) ({game_type})
+
+
+    """).format(game_name = gamename, game_type = gametype, app_name = appname)
+
+    
     
     #Find game's exe file for tracking before post-game syncing (Epic)
     gameexe = ""
@@ -113,18 +125,33 @@ def createlaunchfile(gamename, appname, gamejson, gametype):
 
     """).format(launchcommand = gamecommand[0], offline_launchcommand = gamecommand[1], game_name = gamename) 
 
-    
+    ####################################################################################################################
     #Create final launch script depending on gametype
-    if gametype == "epic":
-        final_launch_script = launch_script + epic_script
+    if configpath.is_flatpak == False:
+        if gametype == "epic":
+            final_launch_script = launch_script + epic_script
+        else:
+            final_launch_script = launch_script + gog_script
     else:
-        final_launch_script = launch_script + gog_script
+        
+        final_launch_script = launch_script
+        
+        if gametype == "epic":
+            flatpak_launch_script = launch_flatpak_script + epic_script 
+        else:
+            flatpak_launch_script = launch_flatpak_script + gog_script 
     
     
     #Write to file
     with open(gameFilepath, "w") as f:
             f.write(final_launch_script)
     os.system("chmod u+x " + gameFilepath)
+
+    if configpath.is_flatpak == True:
+
+        with open(flatpakgamescriptpath, "w") as f:
+            f.write(flatpak_launch_script)
+        os.system("chmod u+x " + flatpakgamescriptpath)
     
     
     #If system is Steam Deck, add to Steam right away or add to Steam script

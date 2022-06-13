93b1f2f952cceb4311c2724c2ced9636c5eac4a7
Redromnon <rishimoharir@gmail.com>
2022-06-13 14:28:59 +0530

    Update to v2.7.3

--- func/HeroicBashLauncher.py
+++ func/HeroicBashLauncher.py
@@ -7,7 +7,7 @@ from func.listinstalled import listinstalled
 from func.steam import createscript, addtosteam
 
 #Print current version
-print("Using Bash Launcher 2.7.2\n")
+print("Using Bash Launcher 2.7.3\nNOTE - This is an independent project and not affiliated with Heroic Games Launcher.\n\n")
 
 #Check if Zenity is installed
 print("Checking if Zenity is installed:")

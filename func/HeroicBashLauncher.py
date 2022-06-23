9851ab6b7bb41f97e6c45a5872df4b68415497b0
Redromnon <rishimoharir@gmail.com>
2022-06-23 12:47:41 +0530

    Update version & check for newer version
    - Notify the user with a dialog box if a newer version is available after setup.sh execution

--- func/HeroicBashLauncher.py
+++ func/HeroicBashLauncher.py
@@ -1,16 +1,35 @@
 #Main file that takes bash arguments
 
-import os,sys
+import os,sys, requests
 from func import configpath
 from func.createlaunchfile import createlaunchfile
 from func.listinstalled import listinstalled
 from func.steam import createscript, addtosteam
 
 #Print current version
-print("Using Bash Launcher 2.7.3\nNOTE - This is an independent project and not affiliated with Heroic Games Launcher.\n\n")
+curr_version = "v2.7.4"
+print("Using Bash Launcher " + curr_version + "\nNOTE - This is an independent project and not affiliated with Heroic Games Launcher.\n")
+
+#Check if a newer version is available
+new_release = False
+checkifonline = True
+
+#Get new version tag if there's an internet connection
+try:
+    release_info = requests.get("https://api.github.com/repos/redromnon/HeroicBashLauncher/releases/latest")
+    new_release_note = ("A newer version " + release_info.json()["tag_name"] + " is available!\n" +
+                        "Please visit https://github.com/redromnon/HeroicBashLauncher/releases to download the latest release.")
+
+    if curr_version != release_info.json()["tag_name"]:
+        new_release = True
+        print(new_release_note)
+except:
+    checkifonline = False
+    print("No internet connection detected.\n\n")
+
 
 #Check if Zenity is installed
-print("Checking if Zenity is installed:")
+print("\nChecking if Zenity is installed...")
 checkzenity = os.system('zenity --version')
 
 if (os.path.exists(configpath.legendaryinstalledpath) == True or os.path.exists(configpath.goginstalledpath) == True) and checkzenity == 0:
@@ -32,6 +51,10 @@ if (os.path.exists(configpath.legendaryinstalledpath) == True or os.path.exists(
             os.system('zenity --info --title="Process Finished" --text="Launch scripts stored in GameFiles folder\n\nYou can choose to add the launch scripts to any game launcher and sync games to Steam via AddToSteam\n\nMake sure to launch newly installed games from Heroic first\n\nHave fun gaming!" --width=300 --timeout=8')
             print("\nCreating AddToSteam script...")
             createscript()
+
+        #Display new release dialog
+        if new_release == True and checkifonline == True:
+            os.system('zenity --info --title="Process Paused" --text="' + new_release_note + '" --width=300')
     elif len(sys.argv) == 2: #Contains simplified gamename as arg for Steam addition
         
         if sys.argv[1] == "":
@@ -51,3 +74,4 @@ elif checkzenity != 0:
 else:
 
     os.system('zenity --error --title="Process Stopped" --text="Looks like you have not installed Heroic Games Launcher or installed any game\n\nPlease consider doing so and try again" --width=300')
+    print("Looks like you have not installed Heroic Games Launcher or installed any game\n\nPlease consider doing so and try again")

0870c4b67fe408f62cc187478b29f8e19ffcf286
Alex Wolfe <3718652+afwolfe@users.noreply.github.com>
2023-01-05 23:50:48 -0500

    Improve checkbinary to attempt to find heroic binary on PATH (#117)
    * Add unittests for checkbinary, refactor a few submodules to be importable
    
    * Update checkbinary to use shutil.which
    
    * Refactor checkbinary to prefer finding Heroic on system path
    
    Fix checkbinary to attempt to find Heroic from symlink on PATH
    
    Reduce redundant if/else checks for gametype in checkbinary
    
    Fix default path checking logic in checkbinary
    
    * Rename TestCheckBinary file, move common mocks to setUp
    
    * Add unittests for checkbinary, refactor a few submodules to be importable
    
    Change gitignore to ignore all __pycache__ folders (incl. tests)
    
    * Update checkbinary to use shutil.which
    
    * Rename TestCheckBinary file, move common mocks to setUp
    
    * Fix flatpak getbinary by removing extra os.path.exists() check
    
    * Simplify checking altLegendaryBin config
    
    * Add unittest to build.yml
    
    Co-authored-by: redromnon <rishimoharir@gmail.com>

--- func/checkbinary.py
+++ func/checkbinary.py
@@ -2,9 +2,11 @@
 #   For AppImage, check if alternavtive binary (Legendary) is added.
 #   If not, check folder under /tmp/ that includes path to the binaries.
 
-import os, json, sys, traceback, logging
+import json, logging, os, shutil, sys, traceback
 from func import configpath
 from func.settings import args
+
+resources_bin_path = "resources/app.asar.unpacked/build/bin/linux"
 
 def getbinary(gametype):
 
@@ -19,38 +21,42 @@ def getbinary(gametype):
 
         #Checking
         binary = ""
+        
+        if gametype != "epic":
+            executable = "gogdl "
+        else:
+            executable = "legendary "
 
-        if os.path.exists("/opt/Heroic/resources/app.asar.unpacked/build/bin/linux") == True:
+        heroic_base_path = None
 
-            if gametype != "epic":
-                binary = "/opt/Heroic/resources/app.asar.unpacked/build/bin/linux/gogdl "
-            else:
-                binary = "/opt/Heroic/resources/app.asar.unpacked/build/bin/linux/legendary "
-        elif 'altLegendaryBin' in heroicconfig['defaultSettings'].keys() and heroicconfig["defaultSettings"]["altLegendaryBin"] != "" and gametype == "epic":
+        # Attempt to find heroic on path
+        detected_heroic_path = shutil.which('heroic')
+        if detected_heroic_path:
+            # Follow any symlinks to the real path of heroic
+            detected_heroic_path = os.path.realpath(detected_heroic_path)
+            if os.path.exists(detected_heroic_path):
+                heroic_base_path = os.path.dirname(detected_heroic_path)
+        elif os.path.exists("/opt/Heroic"): # Default to /opt/Heroic
+            heroic_base_path = "/opt/Heroic"
+        elif configpath.is_flatpak or os.path.exists("/app/bin/heroic"): #System or Flatpak-env path
+            heroic_base_path = "/app/bin/heroic"
+        
+        if heroic_base_path:
+
+            heroic_resources_path = os.path.join(heroic_base_path, resources_bin_path)
+
+            binary = os.path.join(heroic_resources_path, executable)
+        elif heroicconfig["defaultSettings"].get("altLegendaryBin") and gametype == "epic":
 
                 binary = heroicconfig["defaultSettings"]["altLegendaryBin"] + " "
         #elif 'altGogdlBin' in heroicconfig['defaultSettings'].keys() and heroicconfig["defaultSettings"]["altGogdlBin"] != "" and gametype != "epic":
         
                 #binary = heroicconfig["defaultSettings"]["altGogdlBin"] + " "
-        elif configpath.is_flatpak or os.path.exists("/app/bin/heroic"):#System or Flatpak-env path
-
-            if gametype != "epic":
-                binary = "/app/bin/heroic/resources/app.asar.unpacked/build/bin/linux/gogdl "
-            else:
-                binary = "/app/bin/heroic/resources/app.asar.unpacked/build/bin/linux/legendary "
         else:#AppImage
-
             if "GameFiles" in os.getcwd():#select parent dir
-                if gametype != "epic":
-                    binary = os.path.dirname(os.getcwd()) + "/binaries/gogdl "
-                else:
-                    binary =  os.path.dirname(os.getcwd()) + "/binaries/legendary "
+                binary = os.path.join(os.path.dirname(os.getcwd()), "binaries", executable)
             else:
-                if gametype != "epic":
-                    binary = os.getcwd() + "/binaries/gogdl "
-                else:
-                    binary = os.getcwd() + "/binaries/legendary "
-
+                binary = os.path.join(os.getcwd(), "binaries", executable)
             
         return binary
     except Exception:
---
+++ tests/testcheckbinary.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+import os
+import json
+import shutil
+from io import StringIO
+from unittest import main, mock, TestCase
+from unittest.mock import Mock
+
+from func import configpath
+from func.settings import args
+from func import checkbinary
+
+mock_config_null = json.dumps({ "defaultSettings": { "altLegendaryBin": "" } })
+
+mock_config_obj = {"defaultSettings": {"altLegendaryBin": "/config/path/to/legendary"}}
+mock_config_str = json.dumps(mock_config_obj)
+
+class TestCheckBinary(TestCase):
+
+    def setUp(self):
+        # By default which will return nothing (no heroic on PATH)
+        shutil.which = Mock()
+        shutil.which.return_value = None
+
+        # Initialize other mocks
+        os.getcwd = Mock()
+        os.path.exists = Mock()
+        os.path.realpath = Mock()
+
+        # By default we won't test the flatpak path.
+        checkbinary.configpath = Mock() 
+        checkbinary.configpath.is_flatpak = False
+
+        checkbinary.args.silent = True
+
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_null)), create=True)
+    def test_getbinary_epic_opt(self):
+        expected_base_path = "/opt/Heroic"
+        expected_return_path = os.path.join(expected_base_path, checkbinary.resources_bin_path, "legendary ")
+        os.path.exists.side_effect = lambda x: expected_base_path in x
+
+        actual_return_path = checkbinary.getbinary("epic")
+        os.path.exists.assert_any_call(expected_base_path)
+        assert expected_return_path == actual_return_path
+
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_str)), create=True)
+    def test_getbinary_epic_from_config(self):
+        expected_return_path = mock_config_obj["defaultSettings"]["altLegendaryBin"] + " "
+        expected_base_path = os.path.dirname(expected_return_path)
+        os.path.exists.side_effect = lambda x: expected_base_path in x
+
+        actual_return_path = checkbinary.getbinary("epic")
+        assert expected_return_path == actual_return_path
+
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_null)), create=True)
+    def test_getbinary_epic_is_flatpak(self):
+        checkbinary.configpath.is_flatpak = True
+
+        expected_base_path = "/app/bin/heroic"
+        expected_return_path = os.path.join(expected_base_path, checkbinary.resources_bin_path, "legendary ")
+        # Path does not exist on host when using flatpak
+        os.path.exists.return_value = False
+
+        actual_return_path = checkbinary.getbinary("epic")
+        assert expected_return_path == actual_return_path
+
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_null)), create=True)
+    def test_getbinary_epic_app(self):
+        expected_base_path = "/app/bin/heroic"
+        expected_return_path = os.path.join(expected_base_path, checkbinary.resources_bin_path, "legendary ")
+        os.path.exists.side_effect = lambda x: x == expected_base_path or x == os.path.join(expected_base_path, checkbinary.resources_bin_path)
+
+        actual_return_path = checkbinary.getbinary("epic")
+        os.path.exists.assert_any_call(expected_base_path)
+        assert expected_return_path == actual_return_path
+
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_null)), create=True)
+    def test_getbinary_epic_appimage_gamefiles(self):
+        expected_base_path = "/home/user/Games/Heroic/HeroicBashLauncher"
+        os.getcwd.return_value = os.path.join(expected_base_path, "GameFiles")
+        os.path.exists.side_effect = lambda x: expected_base_path in x
+
+        expected_return_path = os.path.join(expected_base_path, "binaries/legendary ")
+        actual_return_path = checkbinary.getbinary("epic")
+        assert expected_return_path == actual_return_path
+
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_null)), create=True)
+    def test_getbinary_epic_appimage(self):
+        expected_base_path = "/home/user/Games/Heroic/HeroicBashLauncher"
+        os.getcwd.return_value = os.path.join(expected_base_path)
+        os.path.exists.side_effect = lambda x: x == expected_base_path or x == os.path.join(expected_base_path, "binaries")
+
+        expected_return_path = os.path.join(expected_base_path, "binaries/legendary ")
+        actual_return_path = checkbinary.getbinary("epic")
+        assert expected_return_path == actual_return_path
+    
+    @mock.patch("builtins.open", Mock(return_value=StringIO(mock_config_null)), create=True)
+    def test_getbinary_epic_on_path(self):
+        shutil.which.side_effect = lambda x: "/usr/lib64/heroic-games-launcher-bin/heroic" if x == "heroic" else None
+        os.path.realpath.side_effect = lambda x: "/usr/lib64/heroic-games-launcher-bin/heroic" if x == "/usr/lib64/heroic-games-launcher-bin/heroic" else x
+        expected_base_path = "/usr/lib64/heroic-games-launcher-bin"
+        expected_return_path = os.path.join(expected_base_path, checkbinary.resources_bin_path, "legendary ")
+        os.path.exists.side_effect = lambda x: expected_base_path in x
+
+        actual_return_path = checkbinary.getbinary("epic")
+        assert expected_return_path == actual_return_path
+
+
+if __name__ == '__main__':
+    main()
